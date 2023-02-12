078267510bdc3c6eaf6a19dc9b5449ade2049f39
SmahiHana <96351416+SmahiHana@users.noreply.github.com>
2023-02-11 18:45:58 -0800

    progress_page_updates

--- js/app_components.js
+++ js/app_components.js
@@ -28,4 +28,13 @@ function newPage(){
   window.location.href = "tbd.html";
 }
 
+//these functions are in progress.html; not sure if needed for future code but put these "tbd" functions just in case/to be more clear
+function tbdLeftArrow(){
+
+}
+
+function tbdRightArrow(){
+
+}
+
 customElements.define('footer-tabs', FooterTabs);
