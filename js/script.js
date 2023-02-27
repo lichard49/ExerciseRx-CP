6065d76ad116d81198492ea994ead7870696f3f0
Richard Li <lichard49@gmail.com>
2023-02-27 00:28:31 -0800

    Fixed spacing in code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Used for Buttons
+function newPage() {
+  window.location.href = "tbd.html";
+}
+
+// these functions are in progress.html; not sure if needed for future code
+// but put these "tbd" functions just in case/to be more clear
+function tbdLeftArrow() {
+
+}
+
+function tbdRightArrow() {
+
+}
